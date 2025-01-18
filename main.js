33a7b44acb2a9727eb1333c17dbde152d1c51439
Guilherme Nascimento <guisantos2005.2013@gmail.com>
2025-01-17 21:25:57 -0300

    finish
    apenas um projeto com jquery

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+        
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const tarefas = $('#input-tarefas').val();
+        const novaTarefa = $('<li></li>');
+        $(`<a class="teste">${tarefas}</a>`).appendTo(novaTarefa);
+        $(novaTarefa).appendTo('ul');
+
+        $('#input-tarefas').val('');
+    })
+
+    $('ul').on('click', 'a', function(){
+        $(this).closest('li').attr('id', 'riscado'); 
+    }) 
+})
